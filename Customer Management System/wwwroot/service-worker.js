2efbabb1e339d4f1c4c4fed3741a51cb2ad51a31
Kuhle Mlinganiso <ST10259861@vcconnect.edu.za>
2025-09-15 00:01:56 +0200

    Added PWA functionality

---
+++ Customer Management System/wwwroot/service-worker.js
@@ -0,0 +1,37 @@
+﻿
+const CACHE_NAME = "cms-cache-v1";
+const urlsToCache = [
+    "/",
+    "/lib/bootstrap/dist/css/bootstrap.min.css",
+    "/css/site.css",
+    "/js/site.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+            );
+        })
+    );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
